15bb23c2e0bc834d49a333246453682600f76b5a
tobias <buecktobias@gmail.com>
2019-12-05 06:08:50 +0100

    created script for day 5

---
+++ src/2019/day5/main.py
@@ -0,0 +1,30 @@
+# Created by Tobias Bück at 2019-12-05 06:08:50.704489
+# Solution of day 5 of advent of Code 2019
+# 
+# INPUTS 
+import utility     # helper methods
+
+def get_input_file():
+    return open("../../../input/2019/input5.txt")
+
+def get_clean_data():
+    with get_input_file() as input_file:
+        lines_input_file = utility.get_lines_of_file(input_file)
+    return lines_input_file    
+
+
+def part1():
+    lines = get_clean_data()
+
+
+def part2():
+    lines = get_clean_data()   
+    
+        
+def main():
+    print(f"part1: " + part1())
+    print(f"part2: " + part2())
+
+
+if __name__ == '__main__':
+    main()    
